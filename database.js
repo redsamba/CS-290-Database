cf35beb0a1b831500ef27eeec24a4c5b915072dc
redsamba <willtimo@oregonstate.edu>
2016-03-18 10:33:54 -0700

    Update database.js

--- database.js
+++ database.js
@@ -107,7 +107,7 @@ app.get('/reset-table',function(req,res,next){
   pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
-    "name VARCHAR(255),"+
+    "name VARCHAR(255) NOT NULL,"+
     "reps INT,"+
     "weight INT,"+
     "date DATE,"+
