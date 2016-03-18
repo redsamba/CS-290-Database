ccb62d7fec8e99837701647ac676f27920d4b29d
redsamba <willtimo@oregonstate.edu>
2016-03-18 11:39:15 -0700

    Update database.js

--- database.js
+++ database.js
@@ -46,10 +46,11 @@ app.get('/',function(req,res,next){
 });
 ////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/insert',function(req,res,next){
-  var context = {};
-
+  
+  console.log(req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs)
+  
   pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES" +  
-  "('Running', '12', '140', '2015-03-15', '1')", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+  "(?, ?, ?, ?, ?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
     if(err){
       next(err);
       return;
